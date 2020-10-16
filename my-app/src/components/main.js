3461237a21a0105fc55b4ff223aa08234aecfbc2
marissa-mayes <marissa.mayes1289@gmail.com>
2020-10-15 23:13:14 -0500

    links to navigation established

--- my-app/src/components/main.js
+++ my-app/src/components/main.js
@@ -1,11 +1,16 @@
 import React from "react";
 import {Switch, Route} from "react-router-dom";
 import homePage from "./homepage";
+import AboutMe from "./about";
+import Projects from "./projects";
+import Contact from "./contact";
 
 const Main = () => (
     <Switch>
         <Route exact path="/" component={homePage}/>
-        
+        <Route  path="/about" component={AboutMe}/>
+        <Route  path="/projects" component={Projects}/>
+        <Route  path="/contact" component={Contact}/>
     </Switch>
 )
 export default Main;
