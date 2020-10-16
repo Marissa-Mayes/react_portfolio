8f12eafb29390f579ff479425e6aa17cc9ef8fc0
marissa-mayes <marissa.mayes1289@gmail.com>
2020-10-16 01:07:45 -0500

    tabs for project created

--- my-app/src/components/projects.js
+++ my-app/src/components/projects.js
@@ -1,11 +1,24 @@
 import React, {Component} from "react" ;
+import {Tabs, Tab} from  "react-mdl";
 
 class Project extends Component{
+    constructor (props) {
+        super(props);
+        this.state ={activeTab: 0 };
+    }
+
     render(){
         return (
-            <div>
-                <h1>PROJECT Page</h1>
-            </div>
+           <div className="category-tabs">
+             <Tabs activeTab={this.state.activeTab} onChange={(tabId)=> this.setState({activeTab:tabId})}ripple>
+                   <Tab>React</Tab>
+                   <Tab>JavaScript</Tab>
+                   <Tab>Express</Tab>
+                   </Tabs>
+                   </div>
+    
+        
+        
         )
     }
 }
