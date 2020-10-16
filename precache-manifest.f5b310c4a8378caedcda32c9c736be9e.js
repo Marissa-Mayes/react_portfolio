b250b225a2ebf0230e62b0e3b1e57b9b07c2cee5
marissa-mayes <marissa.mayes1289@gmail.com>
2020-10-16 03:29:57 -0500

    Updates

---
+++ precache-manifest.f5b310c4a8378caedcda32c9c736be9e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6602ccae8fb5e74beef789cb357c7221",
+    "url": "/react_portfolio/index.html"
+  },
+  {
+    "revision": "90a810b3ac9e8ab455c8",
+    "url": "/react_portfolio/static/css/2.cd1c2086.chunk.css"
+  },
+  {
+    "revision": "723e3ec162e52fff029e",
+    "url": "/react_portfolio/static/css/main.40cb2526.chunk.css"
+  },
+  {
+    "revision": "90a810b3ac9e8ab455c8",
+    "url": "/react_portfolio/static/js/2.00bfc8e0.chunk.js"
+  },
+  {
+    "revision": "8e5ae93e18f66c1d607de05f5b5d011b",
+    "url": "/react_portfolio/static/js/2.00bfc8e0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "723e3ec162e52fff029e",
+    "url": "/react_portfolio/static/js/main.a8584d59.chunk.js"
+  },
+  {
+    "revision": "651c4343a2c615c3ebd6",
+    "url": "/react_portfolio/static/js/runtime-main.3235d0ce.js"
+  }
+]);
